9ea24c12eb20717221381dba3170a12c36586d6d
poojaravi05 <46893746+poojaravi05@users.noreply.github.com>
2025-10-22 17:05:11 -0700

    Create circular_queue_overwrite.c

---
+++ Imp_prac_prob/circular_queue_overwrite.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct cq {
+    int* arr;
+    int sz;
+    int cnt;
+    int rd_ptr;
+    int wr_ptr;
+}cq;
+
+cq* init_buffer(int sz) {
+    cq* queue = (cq*)malloc(sizeof(cq));
+    queue->arr = (int*)malloc(sz * sizeof(int));
+    queue->sz = sz;
+    queue->cnt = 0;
+    queue->rd_ptr = 0;
+    queue->wr_ptr = 0;
+
+    return queue;
+}
+
+void write_queue(cq* queue, int data) {
+    /*
+    if(queue->cnt == queue->sz) {
+        printf("Queue full!\n");
+        return;
+    }
+    */
+    queue->arr[queue->wr_ptr] = data;
+    queue->wr_ptr = (queue->wr_ptr + 1) % queue->sz;
+    queue->cnt++;
+    if(queue->cnt > queue->sz) {
+        queue->cnt = queue->sz;
+    }
+    return;
+}
+
+void read_queue(cq* queue) {
+    if(queue->cnt == 0) {
+        printf("Queue empty!\n");
+        return;
+    }
+    int data = queue->arr[queue->rd_ptr];
+    queue->rd_ptr = (queue->rd_ptr + 1) % queue->sz;
+    queue->cnt--;
+    printf("Data read = %d\n", data);
+    return;
+}
+
+void display_queue(cq* queue) {
+    if(queue->cnt == 0) {
+        printf("Queue empty!\n");
+        return;
+    }
+    int n = queue->cnt;
+    int rd = queue->rd_ptr;
+    printf("Queue elements are: ");
+    while(n) {  //6 5 4 3 2 1 0
+        printf("%d ", queue->arr[rd]); //7 2 3 4 5 6
+        n--;    // 5 4 3 2 1 0
+        rd = (rd + 1) % queue->sz;  //1 2 3 4 5 0
+    }
+    printf("\n");
+    return;
+}
+
+int main() {
+    int size = 6;
+    cq* queue = init_buffer(size);
+
+    write_queue(queue, 1);
+    write_queue(queue, 2);
+    write_queue(queue, 3);
+    write_queue(queue, 4);
+    write_queue(queue, 5);
+    write_queue(queue, 6);
+    write_queue(queue, 7);
+
+    display_queue(queue);
+
+    read_queue(queue);
+    read_queue(queue);
+
+    display_queue(queue);
+    
+    write_queue(queue, 7);
+    
+    display_queue(queue);
+    
+    return 0;
+}
