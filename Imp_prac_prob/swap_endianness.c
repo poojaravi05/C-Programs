9d7436dbc8b0a554ea416d0f1a510ad8ab973bbb
poojaravi05 <46893746+poojaravi05@users.noreply.github.com>
2026-03-16 13:51:19 -0700

    Update swap_endianness.c

--- Imp_prac_prob/swap_endianness.c
+++ Imp_prac_prob/swap_endianness.c
@@ -4,29 +4,19 @@
 int swap_Endians(int value)
 {
 	int leftmost_byte;
-
 	int left_middle_byte;
-
 	int right_middle_byte;
-
 	int rightmost_byte;
-
 	int result;
 
 	leftmost_byte = (value & 0x000000FF) >> 0;
-
 	left_middle_byte = (value & 0x0000FF00) >> 8;
-
 	right_middle_byte = (value & 0x00FF0000) >> 16;
-
 	rightmost_byte = (value & 0xFF000000) >> 24;
-
-	leftmost_byte <<= 24;
-
-	left_middle_byte <<= 16;
-
-	right_middle_byte <<= 8;
 	
+	leftmost_byte <<= 24;
+	left_middle_byte <<= 16;
+	right_middle_byte <<= 8;
 	rightmost_byte <<= 0;
 
 	result = (leftmost_byte | left_middle_byte
