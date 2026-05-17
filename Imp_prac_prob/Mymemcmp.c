37db77618e60ff586dac79659fced69b76adad22
poojaravi05 <46893746+poojaravi05@users.noreply.github.com>
2026-05-16 19:33:10 -0700

    Update Mymemcmp.c

--- Imp_prac_prob/Mymemcmp.c
+++ Imp_prac_prob/Mymemcmp.c
@@ -53,3 +53,21 @@ int main()
 
     return 0;
 }
+
+/*
+int main()
+{
+    int s1[] = {10, 20, 30, 40};
+    int s2[] = {10, 20, 35, 40};
+
+    // Compare first sizeof(s1) bytes of s1 and s2
+    int res = memcmp(s1, s2, sizeof(s1));
+
+    if (res == 0)
+        printf("Arrays are identical");
+    else
+        printf("Arrays differ");
+
+    return 0;
+}
+*/
